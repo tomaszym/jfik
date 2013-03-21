0f628cddac4890143fd66bd41dd4cb63ffb2a58c
Tomek <t.szymula@gmi.pl>
2013-03-21 01:49:05 +0100

    init sbt

---
+++ project/JfikBuild.scala
@@ -0,0 +1,22 @@
+import sbt._
+import sbt.Keys._
+
+object JfikBuild extends Build {
+
+  lazy val jfik = Project(
+    id = "jfik",
+    base = file("."),
+    settings = Project.defaultSettings ++ Seq(
+      name := "jfik",
+      organization := "pl.edu.agh.student.shy",
+      version := "0.12.2",
+      scalaVersion := "2.10.1",
+      libraryDependencies ++= Seq(
+		"com.propensive" %% "rapture-io" % "0.7.2"
+	  ),
+	  resolvers ++= Seq (
+	    "Sonatype OSS Releases" at "https://oss.sonatype.org/content/repositories/releases"
+	  )
+    )
+  )
+}
---
+++ src/main/scala/pl/edu/agh/student/shy/Jfik.scala
@@ -0,0 +1,5 @@
+package pl.edu.agh.student.shy
+
+object Jfik extends App {
+  println("Hello, jfik")
+}
